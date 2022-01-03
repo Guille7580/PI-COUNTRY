eb38fd14cb335aee387d466f1e1a97d917b5dc60
Guille7580 <guilleappen@hotmail.com>
2022-01-03 18:01:19 -0300

    modelos

--- api/index.js
+++ api/index.js
@@ -19,10 +19,38 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const axios = require('axios');
+const {Country} = require('./src/db');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+
+  const countries = await axios.get('https://restcountries.com/v3/all'); 
+
   server.listen(3001, () => {
+
+    
+      countries.data.map(async (c) => {
+			
+        try {
+          await Country.create({
+            id: c.cca3,
+            name: c.name.common,
+            imgBandera: c.flags[1] ? c.flags[1]: 'Image not found',
+            continente: c.region,
+            capital: c.capital ? c.capital[0] : "capital not found",
+            subRegion: c.subregion ? c.subregion : "subregion not found",
+            area: c.area ? c.area : 0,
+            poblacion: c.population ? c.population : 0
+          });
+          
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    
+ 
+  
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+   id:{
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    imgBandera:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continente:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    
+    capital:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subRegion:{
+      type: DataTypes.STRING,
+    },
+    area:{
+      type: DataTypes.STRING,
+    },
+    poblacion:{
+      type: DataTypes.INTEGER,
+    }
   });
 };
